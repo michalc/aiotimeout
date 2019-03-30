c847f1d4769ff3a394345e3419148e5593d38c80
Michal Charemza <michal@charemza.name>
2019-03-30 09:52:27 +0000

    (release) v0.0.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def long_description():
 
 setuptools.setup(
     name='aiotimeout',
-    version='0.0.3',
+    version='0.0.4',
     author='Michal Charemza',
     author_email='michal@charemza.name',
     description='Timeout context manager for asyncio Python',
